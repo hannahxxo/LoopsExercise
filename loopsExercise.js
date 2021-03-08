133d69990827951f21e00515d3ad3f0fe206244a
hannahxxo <hannahantle@gmail.com>
2021-03-08 08:45:13 -0600

    Second Commit

--- loopsExercise.js
+++ loopsExercise.js
@@ -7,3 +7,22 @@ for (i = 1; i <= 7; i++) {
 for (i = 5; i <= 25; i+=4) {
     console.log(i);
 }
+
+// 3a
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+]
+
+// 3b
+
+
+// 4a
+let harryPotterMovies = 0
+
+// 4b
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
